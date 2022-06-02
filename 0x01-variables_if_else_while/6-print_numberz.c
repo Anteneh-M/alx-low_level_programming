5db31b20d1f945a9742953fb626cdbd60f2b959c
Anteneh-M <dmcat234@gmail.com>
2022-06-03 01:52:45 +0300

    done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,21 @@
-#include <stdio.h>                                                                                                                      
-                                                                                                                                        
-/**                                                                                                                                     
-* main - Prints the alphabet in lowercase except q and e.                                                                               
-*                                                                                                                                       
-* Return: Always 0.                                                                                                                     
-*/                                                                                                                                      
-int main(void)                                                                                                                          
-{                                                                                                                                       
-        int n;                                                                                                                          
-                                                                                                                                        
-        for (n = 0; n <= 10;  n++)                                                                                                      
-                {                                                                                                                       
-                putchar("%d", n);                                                                                                        
-                }                                                                                                                       
-        putchar('\n');                                                                                                                  
-        return 0;                                                                                                                       
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success/correct)
+ *
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
+
