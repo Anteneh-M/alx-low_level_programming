edbecbe251bc27511b11097c613eb284ff830bbe
Anteneh-M <dmcat234@gmail.com>
2022-06-02 23:42:37 +0300

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase except q and e.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; letter <= 10;  n++)
+	
+	putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
