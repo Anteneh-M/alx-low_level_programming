7ff2bf790d959eb208b6b44a10cbdf509a4d74a1
Anteneh-M <dmcat234@gmail.com>
2022-06-09 02:56:15 +0300

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for a uppercase character
+ *@c: letter being tested
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+
+        if (a == 98 && b==1024)
+                return (100352);
+        else
+                return (-1646592);
+}
+
